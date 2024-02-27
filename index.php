df772dc33f9fa69eb1e9791dfa5bf09fac95548a
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-27 13:27:46 +0200

    second

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 
+require "function.php";
+
 echo "hi";
 
 $connection_string = "mysql:host=localhost;dbname=blog_pujats;user=root;password=;charset=utf8mb4";
-$PDO = new PDO($connection_string);
+$pdo = new PDO($connection_string);
+
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+//dd($posts);
+
+echo "<ul>";
+foreach($posts as $post) {
+    echo "<li>".$post["title"]."</li>";
+}
+echo "</ul>";
+
+?>
